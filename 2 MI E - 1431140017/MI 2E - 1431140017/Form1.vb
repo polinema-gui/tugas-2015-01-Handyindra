23db6aa92fbd38ae7a3606c5c659383410783f35
Handy Indra <handyindra12@gmail.com>
2015-10-01 21:59:14 +0700

    Tugas Ganti Warna

---
+++ 2 MI E - 1431140017/MI 2E - 1431140017/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Turquoise Then
+            Me.BackColor = Color.Crimson
+        Else : Me.BackColor = Color.Turquoise
+        End If
+    End Sub
+End Class
